5401d9c3265ef6cbefebaac780b071cd6658c14d
Seb85 <sebrenaud@hotmail.fr>
2018-02-14 08:52:43 +0100

    Add files via upload

---
+++ vdm/intent.vdm.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _helpers = require('../../node_modules/ava-ia/lib/helpers');
+
+exports.default = function (state, actions) {
+
+	if (state.isIntent) return (0, _helpers.resolve)(state);
+	
+	var tokens = (0, _helpers.intersect)(Config.modules.vdm.rules, state.tokens);
+
+	if (tokens) {
+		state.isIntent = true;
+		if (state.debug) info('IntentVdm'.bold.green, 'tokens:', tokens.toString().green);
+		return (0, _helpers.factoryActions)(state, actions);
+	} else
+		return (0, _helpers.resolve)(state);
+	
+	
+};
